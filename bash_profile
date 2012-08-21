b356aa9b555bc8bdb73d176be79609c7de660997
Saurav Dhungana <saurav.dh@gmail.com>
2012-08-21 23:37:51 +0300

    Added new dotfiles from mac

---
+++ bash_profile
@@ -0,0 +1,3 @@
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm" # Load RVM function
+[[ -s $HOME/.pythonbrew/etc/bashrc ]] && source $HOME/.pythonbrew/etc/bashrc
+source ~/perl5/perlbrew/etc/bashrc
--- symlink-dotfiles.sh
+++ symlink-dotfiles.sh
@@ -5,26 +5,27 @@
 
 ########## Variables
 
-dir=~/dotfiles                    # dotfiles directory
-olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc vimrc vim"          # list of files/folders to symlink in homedir
+dir=~/dotfiles                                                            # dotfiles directory
+olddir=~/dotfiles/dotfiles_old                                            # old dotfiles backup directory
+files="bashrc bash_profile  gitconfig gitignore_ignore"                   # list of files/folders to symlink in homedir
 
 ##########
 
 # create dotfiles_old in homedir
-echo "Creating $olddir for backup of any existing dotfiles in ~"
-mkdir -p $olddir
-echo "...done"
+#echo "Creating $olddir for backup of any existing dotfiles in ~"
+#mkdir -p $olddir
+#echo "...done"
 
 # change to the dotfiles directory
 echo "Changing to the $dir directory"
 cd $dir
 echo "...done"
 
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
-for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
-    cp ~/.$file ~/dotfiles_old/
+# move any existing dotfiles in homedir to dotfiles_old directory,
+# then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
+  for file in $files; do
+#    echo "Moving any existing dotfiles from ~ to $olddir"
+#    cp ~/.$file $olddir/
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/.$file
 done
